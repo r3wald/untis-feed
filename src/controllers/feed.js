caff4b8b661774003213d3f518d3f11d7d2d4f7b
Robert Ewald <r3wald@gmail.com>
2025-02-26 18:01:29 +0100

    most relevant changes first

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -48,7 +48,7 @@ module.exports = {
             .filter((item) => {
                 return item.resource.date >= dateNotBefore.format('YYYYMMDD');
             })
-            .sort((a, b) => a.readableDate < b.readableDate ? -1 : a.readableDate > b.readableDate ? 1 : 0)
+            .sort((a, b) => a.readableDate > b.readableDate ? -1 : a.readableDate < b.readableDate ? 1 : 0)
         ;
         return res.render('home', {items: items2});
     },
